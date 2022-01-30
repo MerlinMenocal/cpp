1c0ebb8ddd5c6dfd3ec96ad20623f4165e6b6bb0
Merlin Menocal <merlinmm0530@gmail.com>
2022-01-29 22:00:59 -0600

    agregando ejercicio 4

--- Ejercicios/Ejercicio3- Variables/variables.cpp
+++ Ejercicios/Ejercicio3- Variables/variables.cpp
@@ -6,11 +6,11 @@ int main(int argc, char const *argv[])
   int numeroEntero;
 numeroEntero = 15;
 
-cout <<"Entero:"<< numeroEntero;
+cout <<"Entero: "<< numeroEntero;
 cout << endl;
 
 numeroEntero = 18;
-cout << "Entero modificado:"<< numeroEntero;
+cout << "Entero modificado: "<< numeroEntero;
 
     return 0;
 }
