f0ff8ac2e23b46bc4b15c8be707ebe3afc28b3fe
Merlin Menocal <merlinmm0530@gmail.com>
2022-01-29 21:19:52 -0600

    agregando variables

--- Ejercicios/Ejercicio3- Variables/variables.cpp
+++ Ejercicios/Ejercicio3- Variables/variables.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+  int numeroEntero;
+numeroEntero = 15;
 
+cout<<numeroEntero;
+cout<<endl;
 
+numeroEntero = 18;
+cout<<numeroEntero;
 
     return 0;
 }
