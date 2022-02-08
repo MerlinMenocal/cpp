44cdfa0d51b6a100879f3add7384f3826c70fe58
Merlin Menocal <merlinmm0530@gmail.com>
2022-02-07 23:33:40 -0600

    agregando ciclos

---
+++ Ejercicios/IF-ELSE (07-02-2022)/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+int edad1 = 18;
+int edad2 = 10;
+
+// //bool resultado = false;
+
+// //true =1
+// //false = 0
+
+// // true/ false
+// if (edad1 > edad2)
+// {
+//     cout<<"Edad 1 es mayor que Edad 2";   //<--- true
+// }
+// else{
+//     // <-- false
+// }
+        
+//resultado = edad1 > edad2 && edad1 > 18;
+//true/false
+//if (edad1> edad2 && edad1 >18)
+//{
+    /* code */
+//}
+
+while (edad1 > edad2)
+{
+    cout<<"Edad 1 es mayor que Edad 2"<<endl;
+   edad2 = edad2 +1; 
+}
+
+
+    return 0;
+}
