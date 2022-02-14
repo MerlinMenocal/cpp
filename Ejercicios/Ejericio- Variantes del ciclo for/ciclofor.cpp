55ed7c9ef59a03e3e0618a44d9348dfc509bb0ae
Merlin Menocal <merlinmm0530@gmail.com>
2022-02-13 23:08:11 -0600

    agregando variantes del ciclo for

---
+++ Ejercicios/Ejericio- Variantes del ciclo for/ciclofor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    for (int i = 0; i < 10; i++)
+    {
+        cout<<i<<endl;
+    }
+
+    cout<<endl;
+
+    for (int i = 9; i >=0; i--)
+    {
+        cout<<i<<" ";
+    }
+    
+    return 0;
+}
