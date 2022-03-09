7baeb269e4abfffd9c71301fc674ea0db13c3843
Merlin Menocal <merlinmm0530@gmail.com>
2022-03-09 10:35:03 -0600

    cambios

--- Ejercicios_II_Unidad/Ejercicio32-Calculadora-con-Funciones-trwon/main.cpp
+++ Ejercicios_II_Unidad/Ejercicio32-Calculadora-con-Funciones-trwon/main.cpp
@@ -18,6 +18,7 @@ int main(int argc, char const *argv[])
 {
     int n1 = 0;
     int n2 = 0;
+    int resultado = 0;
     char operador;
 
     cout<<"Ingrese a: ";
@@ -31,8 +32,9 @@ int main(int argc, char const *argv[])
 
     try
     {
-        cout<<"Resultado de "<< n1 << operador << n2 << " es: ";
-        cout<<calculadora(n1, n2, operador);
+        resultado = calculadora(n1, n2, operador);
+        cout<<"Resultado de "<< n1 << " " << operador << " " << n2 << " es: ";
+        cout<<resultado;
     }
     catch(const char* error)
     {
