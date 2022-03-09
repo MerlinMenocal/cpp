8a3b2e6f975db6c828f02ad3d5c556b13bcb54d2
Merlin Menocal <merlinmm0530@gmail.com>
2022-03-09 00:22:45 -0600

    cambios

---
+++ Ejercicios_II_Unidad/Ejercicio30-Funciones con Parametros Cin/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int sumar(int a, int b){
+    return a + b;
+}
+
+int restar(int a, int b){
+    return a - b;
+}
+
+int main(int argc, char const *argv[])
+{
+    int numero1 = 0;
+    int numero2 = 0;
+
+    cout<<"Ingrese el Valor de a: "<<endl;
+    cin>>numero1;
+    cout<<"Ingrese el Valor de b: "<<endl;
+    cin>>numero2;
+
+    cout<<endl;
+    cout<<"El resultado de la Suma es: "<<sumar(numero1, numero2);
+    cout<<endl;
+    cout<<"El resultado de la Resta es: "<<restar(numero1, numero2);
+    cout<<endl;
+    
+    return 0;
+}
