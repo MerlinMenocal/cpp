28f0bc66ad0fbfae49552d984fa12df11fe10977
Merlin Menocal <merlinmm0530@gmail.com>
2022-03-09 15:50:13 -0600

    cambios

--- Ejercicios_II_Unidad/Ejercicio45-Punto-de-Ventas-Parte6-Tarea/factura.cpp
+++ Ejercicios_II_Unidad/Ejercicio45-Punto-de-Ventas-Parte6-Tarea/factura.cpp
@@ -3,12 +3,18 @@
 using namespace std;
 
 double subtotal;
+double impuesto = 0.15;
+double total = 0;
+
 string listaProductos;
 
 void agregarProducto(string descripcion, int cantidad, double precio)
 {
     listaProductos = listaProductos + descripcion + '\n';
     subtotal = subtotal + (cantidad * precio);
+    impuesto = subtotal * impuesto;
+    total = subtotal + impuesto;
+
 }
 void imprimirFactura(){
     system("cls");
@@ -21,6 +27,8 @@ void imprimirFactura(){
 
     cout<<endl;
     cout<<"Subtotal: "<<subtotal;
+    cout<<"Impuesto S/V: "<<impuesto;
+    cout<<"Total a Pagar: "<<total;
     cout<<endl;
     cout<<endl;
     system("pause");
