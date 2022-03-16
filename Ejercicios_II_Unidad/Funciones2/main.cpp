eba9440461ab2a0dce846f64220229590df22188
Merlin Menocal <merlinmm0530@gmail.com>
2022-03-16 10:47:58 -0600

    cambios3

--- Ejercicios_II_Unidad/Funciones2/main.cpp
+++ Ejercicios_II_Unidad/Funciones2/main.cpp
@@ -2,23 +2,48 @@
 
 using namespace std;
 
-int calculadora(int a, int b ){
+int calculadora(int a, int b, char o ){
 
+if (o== '+'){
     return a + b;
-
+  }
+  if (o== '-'){
+    return a - b;
+  }
+  if (o== '*'){
+    return a * b;
+  }
+  if (o== '/'){
+    return a / b;
+  }
 }
 
 int main(int argc, char const *argv[])
 {
     int numero1 =0; 
     int numero2 =0;
+    int resultado =0;
+    char operador;
 
     system("cls");
     cout<<"Ingrese en valor del Numero 1: ";
     cin>>numero1;
     cout<<"Ingrese en valor del Numero 2: ";
     cin>>numero2;
+    cout<<endl;
 
-    cout<<calculadora(numero1, numero2);
+    cout<<"Que opecaion desea realizar?";
+    cout<<"============================";
+    cout<<"Ingrese un caracter: ";
+    cout<<"+";
+    cout<<"-";
+    cout<<"*";
+    cout<<"/";
+
+    cin>>operador;
+
+        resultado =calculadora(numero1, numero2, operador);
+
+    //cout<<"Resultado: "<<calculadora(numero1, numero2);
     return 0;
 }
