f0928025482da672a46f3f5268c744caace490ee
Merlin Menocal <merlinmm0530@gmail.com>
2022-03-16 14:03:19 -0600

    agregando tarea

---
+++ Ejercicios_II_Unidad/Ejercicio46-Punto-de-Ventas-archivosH-Tarea2/factura.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+double subtotal =0;
+double impuesto = 0;
+double total = 0;
+
+string listaProductos;
+
+void agregarProducto(string descripcion, int cantidad, double precio)
+{
+    listaProductos = listaProductos + descripcion + '\n';
+    subtotal = subtotal + (cantidad * precio);
+    impuesto =subtotal * 0.15;
+    total = subtotal + impuesto;
+
+}
+void imprimirFactura(){
+    system("cls");
+    cout<<"-------"<<endl;
+    cout<<"FACTURA"<<endl;
+    cout<<endl;
+
+    cout<<"Productos: "<<endl;
+    cout<<listaProductos;
+
+    cout<<endl;
+    cout<<"Subtotal: "<<subtotal<<endl;
+    cout<<"Impuesto S/V: "<<impuesto<<endl;
+    cout<<"Total a Pagar: "<<total<<endl;
+    cout<<endl;
+    cout<<endl;
+    system("pause");
+
+}
