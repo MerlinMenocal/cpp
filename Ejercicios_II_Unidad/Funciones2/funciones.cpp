a1287efb885d69bc51b9122125fa9b948c863230
Merlin Menocal <merlinmm0530@gmail.com>
2022-03-16 11:29:41 -0600

    cambios1.2

---
+++ Ejercicios_II_Unidad/Funciones2/funciones.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+float calculadora(float n1, float n2){
+    return n1 + n2;
+}
+
+int main(int argc, char const *argv[])
+{
+    cout<<calculadora(5, 2);
+    return 0;
+}
+
