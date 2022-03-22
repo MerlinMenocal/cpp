9e23753405e9edb87b74dce9a7b1f7a291e615f8
Merlin Menocal <merlinmm0530@gmail.com>
2022-03-22 16:45:03 -0600

    tarea

---
+++ Ejercicios_II_Unidad/Tarea_8_RaizNumero/raizcuadrada.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <math.h>
+
+//Cree una función que calcule la raiz cuadrada de un número 
+//dado utilizando alguna función de la libreria math.h
+using namespace std;
+
+int raizCuadrada = 4;
+
+int raizNumero(int numero){
+
+return raizCuadrada = sqrt(numero);
+
+}
+
+
+
