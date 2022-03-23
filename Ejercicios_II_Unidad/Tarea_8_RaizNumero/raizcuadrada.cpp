2dce90922289309be79a465003e1907aef3cced2
Merlin Menocal <merlinmm0530@gmail.com>
2022-03-22 22:47:58 -0600

    cambios1

--- Ejercicios_II_Unidad/Tarea_8_RaizNumero/raizcuadrada.cpp
+++ Ejercicios_II_Unidad/Tarea_8_RaizNumero/raizcuadrada.cpp
@@ -6,26 +6,23 @@ using namespace std;
 //Cree una función que calcule la raiz cuadrada de un número 
 //dado utilizando alguna función de la libreria math.h
 
-double sumar(double a, double b){
 
-    return a + b;
+double raizCuadrada = 0;
+double raizNumero(double numero){
+
+    //double numero = 4;
+    //raizCuadrada = sqrt(numero);
+return raizCuadrada = sqrt(numero);
+cout<<"la Raiz Cuadrada es: "<<raizCuadrada;
+
 }
+
 int main(int argc, char const *argv[])
 {
     /* code */
     return 0;
 }
 
-
-// double raizCuadrada = 0;
-// double raizNumero(double numero){
-
-//     //double numero = 4;
-//     //raizCuadrada = sqrt(numero);
-// return raizCuadrada = sqrt(numero);
-// cout<<"la Raiz Cuadrada es: "<<raizCuadrada;
-
-// }
 
 /*int main(int argc, char const *argv[])
 {
