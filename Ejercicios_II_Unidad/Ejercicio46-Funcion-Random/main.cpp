b77ed3752eb3626a8c9bc0917b794989bb1b817f
Merlin Menocal <merlinmm0530@gmail.com>
2022-03-23 19:42:07 -0600

    ejercicio 46

---
+++ Ejercicios_II_Unidad/Ejercicio46-Funcion-Random/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numero = 0;
+
+    //inicializa el numero random
+    srand(time(NULL));
+
+    //genera un numero entre 1 y 10
+    numero = rand() % 10 + 1;
+    cout<<"Primer numero al Azar"<<numero;
+    
+    numero = rand() % 10 + 1;
+    cout<<"Segundo numero al Azar"<<numero;
+    return 0;
+}
