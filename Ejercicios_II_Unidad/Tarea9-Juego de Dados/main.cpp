0e1dbeadc402c3b4c3283a29ddd8c1272876af49
Merlin Menocal <merlinmm0530@gmail.com>
2022-03-29 22:36:12 -0600

    cambios en tarea

--- Ejercicios_II_Unidad/Tarea9-Juego de Dados/main.cpp
+++ Ejercicios_II_Unidad/Tarea9-Juego de Dados/main.cpp
@@ -12,16 +12,17 @@ string jugar;
 
 int main(int argc, char const *argv[])
 {
-    
-    while (true)
-    {
         cout<<"Intrucciones del Juego"<<endl;
         cout<<"----------------------"<<endl;
         cout<<"Ganas cuando: Ambos dados caen en: 6 y 5||5 y 5||3 y 3"<<endl;
         cout<<"Pierdes cuando: Ambos dados caen en: 4 y 4||2 y 2||1 y 2||1 y 1"<<endl<<endl;
+    
+    while (true)
+    {
+        
         cout<<"Presiona la tecla (d) para comenzar: ";
         cin>>jugar;
-        system("pause");
+        system("cls");
 
             for (int i = 0; i <=1; i++)
                 {
