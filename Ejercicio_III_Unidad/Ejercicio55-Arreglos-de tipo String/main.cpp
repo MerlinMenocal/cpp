e36fd13cc15da298898daa46e27d14d76afb3185
Merlin Menocal <merlinmm0530@gmail.com>
2022-04-08 21:06:23 -0600

    cambios

---
+++ Ejercicio_III_Unidad/Ejercicio55-Arreglos-de tipo String/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numeros[]= {4, 5, 8};
+
+    for (int i = 0; i <3; i++)
+    {
+        cout<<numeros[i]<<endl;
+    }
+    
+    return 0;
+}
