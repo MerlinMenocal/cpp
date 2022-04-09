a500042b8c5e019b2c9369f266e15ccde3468637
Merlin Menocal <merlinmm0530@gmail.com>
2022-04-08 20:30:27 -0600

    cambios

---
+++ Ejercicio_III_Unidad/Ejercicio53-Arreglos-Unidimensionales/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+     //        [5]camtidad de elementos(valores) a contener
+    int numeros[5]; //declaracion del arreglo
+
+    //asignacionde valores a un arreglo
+    numeros[0]=10;
+    numeros[1]=5;
+    numeros[2]=15;
+    numeros[3]=7;
+    numeros[4]=20;
+
+    //lectura del arreglo
+    cout<<numeros[0]<<endl;
+    cout<<numeros[1]<<endl;
+    cout<<numeros[2]<<endl;
+    cout<<numeros[3]<<endl;
+    cout<<numeros[4]<<endl;
+
+    return 0;
+}
