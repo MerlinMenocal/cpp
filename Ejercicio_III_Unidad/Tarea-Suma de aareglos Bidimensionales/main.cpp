ea51d9a3510d3134e222d2e69a68127b39ec024b
Merlin Menocal <merlinmm0530@gmail.com>
2022-04-19 16:17:00 -0600

    cambios 11

--- Ejercicio_III_Unidad/Tarea-Suma de aareglos Bidimensionales/main.cpp
+++ Ejercicio_III_Unidad/Tarea-Suma de aareglos Bidimensionales/main.cpp
@@ -108,7 +108,7 @@ for (int columnas = 0; columnas <5; columnas++)
     cout<<endl;
 }
 
-cout<<"Total Suma: "<<(sumaFila + sumaColumna) * sumarArreglo[filas][columnas];
+cout<<"Total Suma: "<<(sumaFila * columnas) + (sumaColumna * filas);
 
     return 0;
 }
