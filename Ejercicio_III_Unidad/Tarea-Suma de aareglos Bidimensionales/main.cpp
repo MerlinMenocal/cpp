b3ffb598c2a17ef5981a237095333004d7a2a579
Merlin Menocal <merlinmm0530@gmail.com>
2022-04-18 23:28:30 -0600

    cambios

--- Ejercicio_III_Unidad/Tarea-Suma de aareglos Bidimensionales/main.cpp
+++ Ejercicio_III_Unidad/Tarea-Suma de aareglos Bidimensionales/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char const *argv[])
     int filas = 5;
     int columnas = 5;
     int resultado = 0;
-     //int total2 = 0;
+     int total1 = 0;
 
     srand(time(NULL));
     int sumarArreglo[filas][columnas]{
@@ -30,31 +30,31 @@ int main(int argc, char const *argv[])
     };
 
   //recorre las filas
-  while (filas ==5)
-  {
+//   while (filas ==5)
+//   {
       for (int i = 0; i <filas; i++)
     {
-        //total1 += filas;
+        total1 += filas;
     }
-       resultado= resultado + sumarArreglo[filas][columnas];
-       filas ++;
-  }
+//        resultado= resultado + sumarArreglo[filas][columnas];
+//        filas ++;
+//   }
   
     
     //recorre las columnas
-    while (columnas ==5)
-    {
+    // while (columnas ==5)
+    // {
         for (int c = 0; c <columnas; c++)
         {
             //cout<<sumarArreglo[i][c];
         }
         
-        resultado= resultado + sumarArreglo[filas][columnas];
-        columnas ++;
-        filas =0;
-    }
+    //     resultado= resultado + sumarArreglo[filas][columnas];
+    //     columnas ++;
+    //     filas =0;
+    // }
 
-    cout<<"Total Suma: "<<resultado;
+    cout<<"Total Suma: "<<sumarArreglo[filas][columnas];
     
     
         
