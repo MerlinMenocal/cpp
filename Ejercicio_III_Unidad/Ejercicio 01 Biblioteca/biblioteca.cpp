aad80b581a60e485cbdceccb5935f8c496a1c8d6
Merlin Menocal <merlinmm0530@gmail.com>
2022-04-25 11:13:17 -0600

    agregando cambios

--- Ejercicio_III_Unidad/Ejercicio 01 Biblioteca/biblioteca.cpp
+++ Ejercicio_III_Unidad/Ejercicio 01 Biblioteca/biblioteca.cpp
@@ -1,19 +1,55 @@
 #include <iostream>
 
 using namespace std;
+string libros[2][2];
 
-string libros[2][2]{
-    {"Algoritmos","Algoritmos y Programacion (Guia para Docentes)"},
-    {"Bases de Datos","Apuntes de Bases de Datos 1"},
-
-};
+void cargarLibros(){
+    libros[0][0]="Algoritmos";
+    libros[0][1]="Algoritmos y Programacion (Guia para Docentes)";
+    
+    libros[1][0]="Bases de Datos";
+    libros[1][1]="Apuntes de Bases de Datos 1";
+}
 
 int main(int argc, char const *argv[])
 {
-    for (int i = 0; i <2; i++)
+    cargarLibros();
+
+    bool salir = false;
+
+    while (salir == false)
     {
-        cout<<libros[i][1]<<endl;
+        string buscar ="";
+        system("cls");
+        cout<<"Ingrese la descripcion del Libro que busca: ";
+        cin>>buscar;
+
+        //Busqueda
+
+        if (salir== false)
+        {
+            char continuar = 'n';
+            while (true)
+            {
+                system("cls");
+                cout<<"No se encontro el Libro que Buscas. Desea continuar? s/n"<<endl;
+                cin>>continuar;
+
+                if (continuar == 's' || continuar == 'S')
+                {
+                    break;
+                }else if(continuar == 'n' || continuar =='N')
+                    salir = true;
+                    break;
+                
+
+            }
+            
+        }
+        
+
     }
     
+
     return 0;
 }
