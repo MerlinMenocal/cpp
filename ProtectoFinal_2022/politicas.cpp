44ba9d626dc5aace0d18b0a72d19adda9d3052ed
Merlin Menocal <merlinmm0530@gmail.com>
2022-04-27 14:28:06 -0600

    cambios 1

--- ProtectoFinal_2022/politicas.cpp
+++ ProtectoFinal_2022/politicas.cpp
@@ -9,13 +9,35 @@ void politicas(){
 
     cout<<"POLITICAS DE LA INSTITUCION"<<endl;
     cout<<"---------------------------"<<endl;
+    cout<<endl;
+    cout<<"POLÍTICA DE CALIDAD"<<endl;
     cout<<"El Colegio Real Royal School es una Institución educativa"<<endl;
-    cout<<"comprometida con la formación integral de los estudiantes,"<<endl;
-    cout<<"con énfasis en el aprendizaje de idiomas (inglés y francés) y "<<endl;
-    cout<<"con un fortalecimiento en valores, en los niveles de Preescolar,"<<endl; 
-    cout<<"Primaria, Secundaria y Media Vocacional."<<endl;
+    cout<<"comprometida con la formacion integral de los estudiantes,"<<endl;
+    cout<<"con enfasis en el aprendizaje de idiomas (ingles y frances)"<<endl;
+    cout<<" y con un fortalecimiento en valores, en los niveles de Preescolar,"<<endl; 
+    cout<<"Primaria y Secundaria."<<endl;
     cout<<endl;
+
+    cout<<"POLÍTICA DE SEGURIDAD Y SALUD EN EL TRABAJO";
     cout<<endl;
+    cout<<"El Colegio Real Royal School establece dentro de sus políticas el compromiso de preservar y mantener al trabajador"<<endl;
+cout<<"en las mejores condiciones de salud, bienestar y equilibrio en su entorno laboral, protegiéndolo de los riesgos generados "<<endl;
+cout<<"por el ambiente y la organización del trabajo a través del Comité Paritario de Seguridad y Salud en el trabajo, dentro de "<<endl;
+cout<<"los objetivos encontramos:"<<endl<<endl;
+
+cout<<"Promover la calidad de vida de nuestros colaboradores a través de la prevención de incidentes, lesiones y enfermedades laborales;"<<endl; 
+cout<<"manteniendo en un nivel aceptable nuestros riesgos más críticos."<<endl;
+cout<<endl;
+cout<<"Respaldar económicamente al sistema de SST, mejorando continuamente nuestras actividades, procesos y servicios."<<endl;
+cout<<endl;
+cout<<"Cumplir con la legislación colombiana vigente y con requisitos de otra índole suscritos por la organización en Seguridad, "<<endl;
+cout<<"salud en el trabajo y protección ambiental."<<endl;
+cout<<endl;
+cout<<"Revisar periódicamente nuestro Sistema de Gestión, para asegurar la pertinencia de los objetivos propuestos; con el propósito"<<endl;
+cout<<"de mejorar continuamente nuestro desempeño en seguridad, salud en el trabajo."<<endl;
+cout<<endl;
+cout<<"Estimular el desarrollo y desempeño hacia la excelencia de nuestro grupo humano; implementado programas de capacitación "<<endl;
+cout<<"en seguridad, salud en el trabajo."<<endl;
 
     system("pause");
 
