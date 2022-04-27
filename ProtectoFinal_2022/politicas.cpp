ca497cfa191f9e61a671b5f4368645f44569aa99
Merlin Menocal <merlinmm0530@gmail.com>
2022-04-27 14:35:46 -0600

    cambios  77

--- ProtectoFinal_2022/politicas.cpp
+++ ProtectoFinal_2022/politicas.cpp
@@ -11,6 +11,7 @@ void politicas(){
     cout<<"---------------------------"<<endl;
     cout<<endl;
     cout<<"POLÍTICA DE CALIDAD"<<endl;
+    cout<<"___________________"<<endl;
     cout<<"El Colegio Real Royal School es una Institución educativa"<<endl;
     cout<<"comprometida con la formacion integral de los estudiantes,"<<endl;
     cout<<"con enfasis en el aprendizaje de idiomas (ingles y frances)"<<endl;
@@ -19,25 +20,24 @@ void politicas(){
     cout<<endl;
 
     cout<<"POLÍTICA DE SEGURIDAD Y SALUD EN EL TRABAJO";
+    cout<<"___________________________________________";
     cout<<endl;
-    cout<<"El Colegio Real Royal School establece dentro de sus políticas el compromiso de preservar y mantener al trabajador"<<endl;
-cout<<"en las mejores condiciones de salud, bienestar y equilibrio en su entorno laboral, protegiéndolo de los riesgos generados "<<endl;
-cout<<"por el ambiente y la organización del trabajo a través del Comité Paritario de Seguridad y Salud en el trabajo, dentro de "<<endl;
-cout<<"los objetivos encontramos:"<<endl<<endl;
+    cout<<"El Colegio Real Royal School establece dentro de sus politicas el compromiso de preservar y mantener al trabajador"<<endl;
+    cout<<"en las mejores condiciones de salud, bienestar y equilibrio en su entorno laboral, protegiendolo de los riesgos generados "<<endl;
+    cout<<"por el ambiente y la organizacion del trabajo a traves del Comite Paritario de Seguridad y Salud en el trabajo, dentro de "<<endl;
+    cout<<"los objetivos encontramos:"<<endl<<endl;
 
-cout<<"Promover la calidad de vida de nuestros colaboradores a través de la prevención de incidentes, lesiones y enfermedades laborales;"<<endl; 
-cout<<"manteniendo en un nivel aceptable nuestros riesgos más críticos."<<endl;
+cout<<"•Promover la calidad de vida de nuestros colaboradores a traves de la prevención de incidentes, lesiones y enfermedades laborales;"<<endl; 
+cout<<"manteniendo en un nivel aceptable nuestros riesgos mas criticos."<<endl;
 cout<<endl;
-cout<<"Respaldar económicamente al sistema de SST, mejorando continuamente nuestras actividades, procesos y servicios."<<endl;
-cout<<endl;
-cout<<"Cumplir con la legislación colombiana vigente y con requisitos de otra índole suscritos por la organización en Seguridad, "<<endl;
+cout<<"Cumplir con la legislacion colombiana vigente y con requisitos de otra índole suscritos por la organizacion en Seguridad, "<<endl;
 cout<<"salud en el trabajo y protección ambiental."<<endl;
 cout<<endl;
-cout<<"Revisar periódicamente nuestro Sistema de Gestión, para asegurar la pertinencia de los objetivos propuestos; con el propósito"<<endl;
-cout<<"de mejorar continuamente nuestro desempeño en seguridad, salud en el trabajo."<<endl;
+cout<<"Revisar periodicamente nuestro Sistema de Gestion, para asegurar la pertinencia de los objetivos propuestos; con el proposito"<<endl;
+cout<<"de mejorar continuamente nuestro desempeno en seguridad, salud en el trabajo."<<endl;
 cout<<endl;
-cout<<"Estimular el desarrollo y desempeño hacia la excelencia de nuestro grupo humano; implementado programas de capacitación "<<endl;
-cout<<"en seguridad, salud en el trabajo."<<endl;
+cout<<"Estimular el desarrollo y desempeno hacia la excelencia de nuestro grupo humano; implementado programas de capacitacion "<<endl;
+cout<<"en seguridad y salud en el trabajo."<<endl<<endl;
 
     system("pause");
 
