c669959317934bb87c8b0aa344544a68fbef0ca0
Merlin Menocal <merlinmm0530@gmail.com>
2022-04-27 16:33:25 -0600

    cambios

--- ProtectoFinal_2022/estudiantes.cpp
+++ ProtectoFinal_2022/estudiantes.cpp
@@ -7,7 +7,7 @@ void estudiantes(){
         int opcion = 0;
         cout<<"1- Ingresar un Estudiante"<<endl;
         cout<<"2- Eliminar un Estudiante"<<endl;
-        cout<<"0"<<endl;
+        cout<<"3- Volver al Menu"<<endl;
 
         cout<<"Ingrese un Numero del menu y presione enter --> ";
         cin>>opcion;
@@ -15,7 +15,7 @@ void estudiantes(){
         switch (opcion)
         {
         case 1:
-            //ingresoEstudiante();
+            ingresoEstudiantes();
             break;
         case 2:
             //eliminarEstudiante();
@@ -29,7 +29,44 @@ void estudiantes(){
         default:
             break;
         }
-
         };
+
+    string ingresoEstudiantes[10][6];
+        
+        
+        for (int i = 0; i <10; i++)
+    {
+        cout<<"Ingreso de Datos para Estudiantes"<<endl;
+        cout<<"---------------------------------"<<endl;
+
+        cout<<"Ingrese el Nombre: ";
+        cin>>persona[i][0];
+        cout<<endl;
+
+        cout<<"Ingrese el Apellido: ";
+        cin>>persona[i][1];
+        cout<<endl;
+
+        cout<<"Ingrese el ID: ";
+        cin>>persona[i][2];
+        cout<<endl;
+
+        cout<<"Ingrese el Telefono de Contacto: ";
+        cin>>persona[i][3];
+        cout<<endl;
+
+        cout<<"Ingrese la edad: ";
+        cin>>persona[i][4];
+        cout<<endl;
+
+        cout<<"Ingrese el correo Electronico: ";
+        cin>>persona[i][5];
+        cout<<endl;
+
+    }
+    
+
+    
+    
     
 
