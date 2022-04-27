8c0850bf69cff99c5ebad3e86c87bcab0d3898f8
Merlin Menocal <merlinmm0530@gmail.com>
2022-04-27 14:42:01 -0600

    cambios 44

--- ProtectoFinal_2022/politicas.cpp
+++ ProtectoFinal_2022/politicas.cpp
@@ -7,10 +7,10 @@ using namespace std;
 void politicas(){
     setlocale(LC_ALL, "es_ES");//Idioma y acentos
 
-    cout<<"POLITICAS DE LA INSTITUCION"<<endl;
+    cout<<"\t\tPOLITICAS DE LA INSTITUCION"<<endl;
     cout<<"---------------------------"<<endl;
     cout<<endl;
-    cout<<"POLÍTICA DE CALIDAD"<<endl;
+    cout<<"POLITICA DE CALIDAD"<<endl;
     cout<<"___________________"<<endl;
     cout<<"El Colegio Real Royal School es una Institución educativa"<<endl;
     cout<<"comprometida con la formacion integral de los estudiantes,"<<endl;
@@ -19,8 +19,8 @@ void politicas(){
     cout<<"Primaria y Secundaria."<<endl;
     cout<<endl;
 
-    cout<<"POLITICA DE SEGURIDAD Y SALUD EN EL TRABAJO";
-    cout<<"___________________________________________"<<endl;
+    cout<<"POLITICA DE SEGURIDAD Y SALUD EN EL TRABAJO"<<endl;
+    cout<<"___________________________________________";
     cout<<endl;
     cout<<"El Colegio Real Royal School establece dentro de sus politicas el compromiso de preservar y mantener al trabajador"<<endl;
     cout<<"en las mejores condiciones de salud, bienestar y equilibrio en su entorno laboral, protegiendolo de los riesgos generados "<<endl;
@@ -30,7 +30,7 @@ void politicas(){
 cout<<"•Promover la calidad de vida de nuestros colaboradores a traves de la prevencion de incidentes, lesiones y enfermedades laborales;"<<endl; 
 cout<<"manteniendo en un nivel aceptable nuestros riesgos mas criticos."<<endl;
 cout<<endl;
-cout<<"Cumplir con la legislacion colombiana vigente y con requisitos de otra índole suscritos por la organizacion en Seguridad, "<<endl;
+cout<<"Cumplir con la legislacion colombiana vigente y con requisitos de otra indole suscritos por la organizacion en Seguridad, "<<endl;
 cout<<"salud en el trabajo y proteccion ambiental."<<endl;
 cout<<endl;
 cout<<"Revisar periodicamente nuestro Sistema de Gestion, para asegurar la pertinencia de los objetivos propuestos; con el proposito"<<endl;
