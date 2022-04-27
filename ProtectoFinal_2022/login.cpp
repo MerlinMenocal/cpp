2b19b2c6a5185fb65af71cfd66302278bf33a81f
Merlin Menocal <merlinmm0530@gmail.com>
2022-04-27 11:55:07 -0600

    cambios 5

--- ProtectoFinal_2022/login.cpp
+++ ProtectoFinal_2022/login.cpp
@@ -10,19 +10,18 @@ bool login(){
 	int intentos = 0;
 
 while (intentos <=3){
-      //system("cls");
+      system("cls");
 
 		if (intentos ==3)
-
 		{
 			cout<<"Usuario o Contrasena incorrectos!"<<endl;
 			return false;
 		}
 	
 	cout<<endl;
-	cout<<"\t\t\t____________________________"<<endl;
-	cout<<"\t\t\t SISTEMA DE NOTAS ESCOLARES "<<endl;
-	cout<<"\t\t\t****************************";
+	cout<<"\t\t____________________________"<<endl;
+	cout<<"\t\t SISTEMA DE NOTAS ESCOLARES "<<endl;
+	cout<<"\t\t****************************";
 	cout<<endl;
 	cout<<endl;
 	
