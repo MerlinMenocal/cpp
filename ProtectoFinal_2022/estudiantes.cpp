3dbf70291bd794e077df283d4090bf792d09e583
Merlin Menocal <merlinmm0530@gmail.com>
2022-04-28 15:14:06 -0600

    cambios 99

--- ProtectoFinal_2022/estudiantes.cpp
+++ ProtectoFinal_2022/estudiantes.cpp
@@ -22,7 +22,7 @@ void listaAlumnos (){
     cout<<"LISTADO DE ALUMNOS"<<endl;
     cout<<"********************"<<endl;
 
-    for (int i = 0; i <5; i++)
+    for (int i = 0; i <3; i++) //imprime solo 3 alumnos para efectos de prueba
         {
             cout<<"Codigo: "<<alumnos[i][0]<<endl;
             cout<<"Nombre: "<<alumnos[i][1]<<endl;
